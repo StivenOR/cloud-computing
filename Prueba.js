f9e308cb7f75d958be4f45100a5f6eb033a8471e
Stiven Osorio <stivenosorioroldan@gmail.com>
2024-03-08 16:27:53 -0500

    2do commit

---
+++ Prueba.js
@@ -0,0 +1,27 @@
+function suma(a, b) {
+    return a + b;
+  }
+  
+  function resta(a, b) {
+    return a - b;
+  }
+  
+  function multiplicacion(a, b) {
+    return a * b;
+  }
+  
+  function division(a, b) {
+    return a / b;
+  }
+  
+  // Ejemplo de uso
+  let resultadoSuma = suma(5, 3);
+  let resultadoResta = resta(10, 4);
+  let resultadoMultiplicacion = multiplicacion(2, 5);
+  let resultadoDivision = division(12, 3);
+  
+  console.log("Suma:", resultadoSuma);
+  console.log("Resta:", resultadoResta);
+  console.log("Multiplicación:", resultadoMultiplicacion);
+  console.log("División:", resultadoDivision);
+  
